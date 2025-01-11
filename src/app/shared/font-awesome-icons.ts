4939910b40f7062c0b409635e7fcb09714abbb5f
Bartlomiej Noworyta <101003585+B4JD1K@users.noreply.github.com>
2025-01-11 22:19:02 +0100

    updated IconDefinition

--- src/app/shared/font-awesome-icons.ts
+++ src/app/shared/font-awesome-icons.ts
@@ -1,9 +1,10 @@
 import {IconDefinition} from "@fortawesome/fontawesome-svg-core";
 import {faAirbnb} from "@fortawesome/free-brands-svg-icons";
-import {faSearch, faUser} from "@fortawesome/free-solid-svg-icons";
+import {faBars, faSearch, faUser} from "@fortawesome/free-solid-svg-icons";
 
 export const fontAwesomeIcons: IconDefinition[]=[
   faAirbnb,
   faUser,
-  faSearch
+  faSearch,
+  faBars
 ]
