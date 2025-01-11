651083b40b387befad877042b44f1a9bae8b75de
Bartlomiej Noworyta <101003585+B4JD1K@users.noreply.github.com>
2025-01-11 16:08:29 +0100

    created components to html file, that will be responsible for steering avatar images

--- src/app/layout/navbar/avatar/avatar.component.ts
+++ src/app/layout/navbar/avatar/avatar.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, input} from '@angular/core';
+import {NgClass} from "@angular/common";
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 
 @Component({
   selector: 'app-avatar',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass,
+    FontAwesomeModule
+  ],
   templateUrl: './avatar.component.html',
   styleUrl: './avatar.component.scss'
 })
 export class AvatarComponent {
-
+  // url avatara użytkownika - jeśli pusty lub null to ustawiona zostanie domyślna ikona
+  imageUrl = input<string>();
+  // rozmiar avatara, przykazywany jako klasa do CSS
+  avatarSize = input<"avatar-sm" | "avatar-xl">();
 }
