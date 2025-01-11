9ac70fe3a4db61c3f63028a809aa24420334db35
Bartlomiej Noworyta <101003585+B4JD1K@users.noreply.github.com>
2025-01-11 21:12:45 +0100

    defined names for buttons (location, guests, dates), prepared methods for login and logout, initialized fetchMenu

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ButtonModule} from "primeng/button";
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {ToolbarModule} from "primeng/toolbar";
+import {MenuModule} from "primeng/menu";
+import {DialogService} from "primeng/dynamicdialog";
+import {MenuItem} from "primeng/api";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonModule,
+    FontAwesomeModule,
+    ToolbarModule,
+    MenuModule
+  ],
+  providers: [DialogService],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
+  location = "Anywhere";
+  guests = "Add guests";
+  dates = "Any week";
+
+  // to do
+  // login () => this.authService.login();
+  //
+  // logout () => this.authService.logout();
+
+  currentMenuItems: MenuItem[] | undefined = [];
+
+  ngOnInit() {
+    this.fetchMenu();
+  }
+
+  private fetchMenu() {
+    return [
+      {
+        label: "Sign up",
+        styleClass: "font-bold"
+      }, {
+        label: "Log in",
+      }
+    ]
+  }
 }
