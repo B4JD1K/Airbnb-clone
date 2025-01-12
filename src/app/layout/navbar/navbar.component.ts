1116693183c7bdc30969a1446103ca2bf17f8d53
Bartlomiej Noworyta <101003585+B4JD1K@users.noreply.github.com>
2025-01-12 02:35:21 +0100

    injected toastService and displayed toast at main page

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {ButtonModule} from "primeng/button";
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import {ToolbarModule} from "primeng/toolbar";
@@ -7,6 +7,7 @@ import {DialogService} from "primeng/dynamicdialog";
 import {MenuItem} from "primeng/api";
 import {AvatarComponent} from "./avatar/avatar.component";
 import {CategoryComponent} from "./category/category.component";
+import {ToastService} from "../toast.service";
 
 @Component({
   selector: 'app-navbar',
@@ -29,6 +30,9 @@ export class NavbarComponent implements OnInit {
   guests = "Add guests";
   dates = "Any week";
 
+  // wstrzyknięcie zależności ToastService
+  toastService = inject(ToastService);
+
   // to do
   // login () => this.authService.login();
   //
@@ -38,6 +42,8 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit() {
     this.fetchMenu();
+    // wysłanie wiadomości toast o podanej treści z rodzajem(poziomem) 'info'
+    this.toastService.send({severity: "info", summary: "Welcome to your airbnb app!"})
   }
 
   private fetchMenu() {
