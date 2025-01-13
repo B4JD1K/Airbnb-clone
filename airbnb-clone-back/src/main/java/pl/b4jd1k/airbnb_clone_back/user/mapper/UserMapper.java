ede688697b7ef3162804ff2f4f8cd0c6b943a3c6
Bartlomiej Noworyta <101003585+B4JD1K@users.noreply.github.com>
2025-01-13 14:22:00 +0100

    created UserMapper for UserDTO

---
+++ airbnb-clone-back/src/main/java/pl/b4jd1k/airbnb_clone_back/user/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package pl.b4jd1k.airbnb_clone_back.user.mapper;
+
+import org.mapstruct.Mapper;
+
+// MapStruct automatycznie wygeneruje implementacje, która będzie konwertować obiekty między klasą 'User' a jej DTO
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+}
