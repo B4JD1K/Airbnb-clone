5905de7ba38a826dabb2354ea9871d3f7d81f505
Bartlomiej Noworyta <101003585+B4JD1K@users.noreply.github.com>
2025-01-13 22:52:08 +0100

    moved to repository package

--- airbnb-clone-back/src/main/java/pl/b4jd1k/airbnb_clone_back/booking/BookingRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.b4jd1k.airbnb_clone_back.booking;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.b4jd1k.airbnb_clone_back.booking.domain.Booking;
-
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-}
