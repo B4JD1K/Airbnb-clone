788bdba1e4df67294ffe69a87012a22eb1a40ae5
Bartlomiej Noworyta <101003585+B4JD1K@users.noreply.github.com>
2025-01-13 14:44:57 +0100

    created mapper for ListingPicture entity

---
+++ airbnb-clone-back/src/main/java/pl/b4jd1k/airbnb_clone_back/listing/mapper/ListingPictureMapper.java
@@ -0,0 +1,8 @@
+package pl.b4jd1k.airbnb_clone_back.listing.mapper;
+
+import org.mapstruct.Mapper;
+
+// generowany przez MapStruct, służy do konwertowania obiektów ListingPicture na DTO i odwrotnie
+@Mapper(componentModel = "spring")
+public interface ListingPictureMapper {
+}
