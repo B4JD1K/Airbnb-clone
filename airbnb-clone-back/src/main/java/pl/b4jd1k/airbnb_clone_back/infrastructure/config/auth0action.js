c390b35a8b7673a5c598299a1373e7e905d9add4
Bartlomiej Noworyta <101003585+B4JD1K@users.noreply.github.com>
2025-01-14 17:03:22 +0100

    auth0 action login flow - code written on manage.auth0.com website, working with "okta:oauth2" in application.yml + comments

--- airbnb-clone-back/src/main/java/pl/b4jd1k/airbnb_clone_back/infrastructure/config/auth0action.js
+++ airbnb-clone-back/src/main/java/pl/b4jd1k/airbnb_clone_back/infrastructure/config/auth0action.js
@@ -6,34 +6,46 @@
 * @param {Event} event - Details about the user and the context in which they are logging in.
 * @param {PostLoginAPI} api - Interface whose methods can be used to change the behavior of the login.
 */
+
+// handler wywoływany w trakcie przepływu PostLogin (po udanym logowaniu)
+// - event: zawiera szczegóły dotyczące użytkownika i kontekstu logowania
+//   - dane usera: event.user (np. user_id, email, metadata)
+//   - role usera: event.authorization.roles (jeśli przypisane w Auth0 Dashboard)
+//   - kontekst logowania: event.transaction (np. protokół logowania - OAuth2/SAML, język, metoda logowania)
+// - api: interfejs do modyfikacji procesu logowania
+//   - umożliwia np. dodawanie niestandardowych claims do tokenów
 exports.onExecutePostLogin = async (event, api) => {
   const namespace = "https://www.b4jd1k.pl"
 
+  // jeśli user ma przypisane role w `event.authorization`, dodaj je do tokenów
   if (event.authorization && event.authorization.roles && event.authorization.roles.length > 0){
     if(event.authorization){
-      api.idToken.setCustomClaim(`${namespace}/roles`, event.authorization.roles);
-      api.accessToken.setCustomClaim(`${namespace}/roles`, event.authorization.roles);
+      api.idToken.setCustomClaim(`${namespace}/roles`, event.authorization.roles); // dodaj role do id token
+      api.accessToken.setCustomClaim(`${namespace}/roles`, event.authorization.roles); // dodaj role do access token
     }
-    return
+    return // zakończenie działania, bo role zostały dodane
   }
 
+  // jeśli użytkownik nie ma ról, przypisz rolę za pomocą Management API i zaktualizuj tokeny
   const ManagementClient = require('auth0').ManagementClient;
 
+  // dane uwierzytelniające dla Management API
   const management = new ManagementClient({
     domain: event.secrets.DOMAIN,
     clientId: event.secrets.CLIENT_ID,
     clientSecret: event.secrets.CLIENT_SECRET,
   })
 
-  const params = {id: event.user.user_id};
-  const data = {"roles":["rol_LiAXp9UWq9nYeXa3"]}
+  const params = {id: event.user.user_id}; // id użytkownika
+  const data = {"roles":["rol_LiAXp9UWq9nYeXa3"]} // rola do przypisania
 
+  // pozwalają na dodanie niestandardowych claims (np. role użytkownika) do tokenów ID i Access
   try{
-    await management.users.assignRoles(params, data);
-    const rolesData = await management. users.getRoles(params);
-    const rolesName = rolesData.data.map(role => role.name);
-      api.idToken.setCustomClaim(`${namespace}/roles`, rolesName);
-      api.accessToken.setCustomClaim(`${namespace}/roles`, rolesName);
+    await management.users.assignRoles(params, data); // przypisanie roli użytkownikowi
+    const rolesData = await management. users.getRoles(params); // pobranie ról użytkownika
+    const rolesName = rolesData.data.map(role => role.name); // mapowanie nazwy ról
+      api.idToken.setCustomClaim(`${namespace}/roles`, rolesName); // ustawienie claims w tokenach (id)
+      api.accessToken.setCustomClaim(`${namespace}/roles`, rolesName); // - || - (Access)
   } catch (e) {
     console.log(e);
   }
