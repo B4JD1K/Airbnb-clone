cb51537f4a5a385d29b3253ed0fd636b4db205f3
Bartlomiej Noworyta <101003585+B4JD1K@users.noreply.github.com>
2025-01-15 08:08:47 +0100

    added AuthResource with OAuth2 user synchronization and logout functionality

---
+++ airbnb-clone-back/src/main/java/pl/b4jd1k/airbnb_clone_back/user/presentation/AuthResource.java
@@ -0,0 +1,54 @@
+package pl.b4jd1k.airbnb_clone_back.user.presentation;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.*;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pl.b4jd1k.airbnb_clone_back.user.application.UserService;
+import pl.b4jd1k.airbnb_clone_back.user.application.dto.ReadUserDTO;
+
+import java.text.MessageFormat;
+import java.util.Map;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthResource {
+
+  private final UserService userService;
+  private final ClientRegistration registration;
+
+  public AuthResource(UserService userService, ClientRegistrationRepository registration) {
+    this.userService = userService;
+    this.registration = registration.findByRegistrationId("okta"); // określenie "dostawcy" sposobu logowania (pośrednika - google, gh, fb)
+  }
+
+  @GetMapping("/get-authenticated-user")
+  public ResponseEntity<ReadUserDTO> getAuthenticatedUser(
+    @AuthenticationPrincipal OAuth2User user, @RequestParam boolean forceResync) {
+    // sprawdzenie czy użytkownik jest zalogowany
+    if (user == null) {
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    } else {
+      userService.syncWithIdp(user, forceResync); // synchronizacja danych zalogowanego użytkownika
+      ReadUserDTO connectedUser = userService.getAuthenticatedUserFromSecurityContext(); // pobranie jego danych
+      return new ResponseEntity<>(connectedUser, HttpStatus.OK); // odpowiedź ResponseEntity z danymi użytkownika i kodem 200
+    }
+  }
+
+  // obsługa wylogowania użytkownika z aplikacji i idp
+  public ResponseEntity<Map<String, String>> logout(HttpServletRequest request) {
+    String issuerUri = registration.getProviderDetails().getIssuerUri(); // pobiera adres URL dostawcy tożsamości (idp)
+    String originUri = request.getHeader(HttpHeaders.ORIGIN); // protokół+domena
+    Object[] params = {issuerUri, registration.getClientId(), originUri};
+    String logoutUrl = MessageFormat.format("{0}v2/logout?client_id={1}&returnTo={2}", params);
+    request.getSession().invalidate(); // unieważnienie obecnej sesji użytkownika na serwerze
+    return ResponseEntity.ok().body(Map.of("logoutUrl", logoutUrl));
+  }
+}
