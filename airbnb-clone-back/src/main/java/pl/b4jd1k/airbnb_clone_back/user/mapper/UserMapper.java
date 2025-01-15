40b4e5a04153f0f67a807fb0a96299c9d5491d81
Bartlomiej Noworyta <101003585+B4JD1K@users.noreply.github.com>
2025-01-15 07:06:08 +0100

    defined automatic conversion User objects to ReadUserDTO, mapping Authority into String

--- airbnb-clone-back/src/main/java/pl/b4jd1k/airbnb_clone_back/user/mapper/UserMapper.java
+++ airbnb-clone-back/src/main/java/pl/b4jd1k/airbnb_clone_back/user/mapper/UserMapper.java
@@ -1,8 +1,19 @@
 package pl.b4jd1k.airbnb_clone_back.user.mapper;
 
 import org.mapstruct.Mapper;
+import pl.b4jd1k.airbnb_clone_back.user.application.dto.ReadUserDTO;
+import pl.b4jd1k.airbnb_clone_back.user.domain.Authority;
+import pl.b4jd1k.airbnb_clone_back.user.domain.User;
 
 // MapStruct automatycznie wygeneruje implementacje, która będzie konwertować obiekty między klasą 'User' a jej DTO
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+
+  ReadUserDTO readUserDTOToUser(User user); // mapuje obiekt User na ReadUserDTO
+
+  // dla każdego obiektu 'Authority' zwróci nazwę roli
+  // MapStruct dzięki tej metodzie skonwertuje kolecjkę Set<Authority> (z klasy User) na Set<String> (ReadUserDTO)
+  default String mapAuthoritiesToString(Authority authority){
+    return authority.getName();
+  }
 }
