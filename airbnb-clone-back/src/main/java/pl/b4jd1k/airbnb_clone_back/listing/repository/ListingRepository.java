df2772c46aa6e6f5b57de583dfbe89f6b510314e
Bartlomiej Noworyta <101003585+B4JD1K@users.noreply.github.com>
2025-01-22 23:03:54 +0100

    feat(listing): add category-based listing retrieval with pagination in homepage
    - Add new endpoint /api/tenant-listing/get-all-by-category
    - Implement repository methods for fetching listings with cover pictures
    - Add service layer implementation for category filtering
    - Support ALL category to fetch all listings

--- airbnb-clone-back/src/main/java/pl/b4jd1k/airbnb_clone_back/listing/repository/ListingRepository.java
+++ airbnb-clone-back/src/main/java/pl/b4jd1k/airbnb_clone_back/listing/repository/ListingRepository.java
@@ -1,17 +1,29 @@
 package pl.b4jd1k.airbnb_clone_back.listing.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import pl.b4jd1k.airbnb_clone_back.listing.domain.BookingCategory;
 import pl.b4jd1k.airbnb_clone_back.listing.domain.Listing;
 
 import java.util.List;
 import java.util.UUID;
 
-public interface ListingRepository extends JpaRepository<Listing,Long> {
+public interface ListingRepository extends JpaRepository<Listing, Long> {
 
-@Query("SELECT listing FROM Listing listing LEFT JOIN FETCH listing.pictures picture" +
-        " WHERE listing.landlordPublicId = :landlordPublicId AND picture.isCover = true")
+  @Query("SELECT listing FROM Listing listing LEFT JOIN FETCH listing.pictures picture" +
+    " WHERE listing.landlordPublicId = :landlordPublicId AND picture.isCover = true")
   List<Listing> findAllByLandlordPublicIdFetchCoverPicture(UUID landlordPublicId);
 
   long deleteByPublicIdAndLandlordPublicId(UUID publicId, UUID landlordPublicId);
+
+  // pageable - poniewaz nie chcemy aby wszystkie rekordy z bazy sie ladowaly bo byloby to za duzo danych
+  @Query("SELECT listing FROM Listing listing LEFT JOIN FETCH listing.pictures picture" +
+    " WHERE picture.isCover = true AND listing.bookingCategory = :bookingCategory")
+  Page<Listing> findAllByBookingCategoryWithCoverOnly(Pageable pageable, BookingCategory bookingCategory);
+
+  @Query("SELECT listing FROM Listing listing LEFT JOIN FETCH listing.pictures picture" +
+    " WHERE picture.isCover = true")
+  Page<Listing> findAllWithCoverOnly(Pageable pageable);
 }
