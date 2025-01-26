f06744a8a0afdf8bf7edd8baa12354de8ef13206
B4JD1K <bartlomiej.noworyta.96@gmail.com>
2025-01-26 10:22:57 +0100

    feat(ui): integrate search with navbar and map
    - Add search controls to navigation bar
    - Update map component to reflect search results
    - Integrate search with tenant listing service

--- airbnb-clone-front/src/app/tenant/tenant-listing.service.ts
+++ airbnb-clone-front/src/app/tenant/tenant-listing.service.ts
@@ -5,6 +5,8 @@ import {State} from "../core/model/state.model";
 import {createPaginationOption, Page, Pagination} from "../core/model/request.model";
 import {CategoryName} from "../layout/navbar/category/category.model";
 import {environment} from "../../environments/environment";
+import {Subject} from "rxjs";
+import {Search} from "./search/search.model";
 
 @Injectable({
   providedIn: 'root'
@@ -21,6 +23,29 @@ export class TenantListingService {
     signal(State.Builder<Listing>().forInit());
   getOneByPublicIdSig = computed(() => this.getOneByPublicId$());
 
+  /**
+   * Powyższy Sygnał jest używany do przechowywania i zarządzania stanem reaktywnie.
+   * Sygnały mogą być odczytywane, zapisywane, a także automatycznie powiadamiają subskrybentów o zmianachh stanu.
+   * Przykładowo:
+   * WritableSignal<State<Listing>> jest używany do zarządzania stanem pojedynczego ogłoszenia,
+   * pozwalając komponentom na reaktywne aktualizowanie się, gdy stan się zmienia
+   *
+   * Natomiast poniższy Subject jest typem obserwowalnym (z RxJS), ale nie jest używany do przechowywania stanu.
+   * Jest używany do emitowania wartości do wielu subskrybentów.
+   * Działają zarówno jako obserwator, jak i obserwowany, co oznacza, że mogą emitowaćnowe wartości i być subsrybowane.
+   * Przykładowo:
+   * Subject<State<Page<CardListing>>> jest używany do emitowania stanu operacji wyszukiwania,
+   * pozwalając wielu subskrybentom reagować na wyemitowane wartości.
+   *
+   * Podsumowanie:
+   * WritableSignal jest używany do przechowywania i zarządzania stanem reaktywnie.
+   * Subject jest używany do emitowania wartości do wielu subskrybentów.
+   */
+
+  private search$: Subject<State<Page<CardListing>>> =
+    new Subject<State<Page<CardListing>>>();
+  search = this.search$.asObservable();
+
   constructor() {
   }
 
@@ -34,7 +59,7 @@ export class TenantListingService {
       })
   }
 
-  resetGetAllCategory():void{
+  resetGetAllCategory(): void {
     this.getAllByCategory$.set(State.Builder<Page<CardListing>>().forInit());
   }
 
@@ -47,7 +72,16 @@ export class TenantListingService {
       })
   }
 
-  resetGetOneByPublicId():void{
+  resetGetOneByPublicId(): void {
     this.getOneByPublicId$.set(State.Builder<Listing>().forInit());
+  }
+
+  searchListing(newSearch: Search, pageRequest: Pagination): void {
+    const params = createPaginationOption(pageRequest);
+    this.http.post<Page<CardListing>>(`${environment.API_URL}/tenant-listing/search`, newSearch, {params})
+      .subscribe({
+        next: displayListingCards => this.search$.next(State.Builder<Page<CardListing>>().forSuccess(displayListingCards)),
+        error: err => this.search$.next(State.Builder<Page<CardListing>>().forError(err))
+      })
   }
 }
